e5dc4c78f29b4a53ef2837b04f2112a7e15ebd4c
Stuart Geipel <lapimlu@gmail.com>
2021-09-06 14:47:39 -0700

    fix prod build with web workers

--- vite/src/ts/wasm.ts
+++ vite/src/ts/wasm.ts
@@ -1,4 +1,7 @@
-import init from '../../../stubot-wasm/pkg/stubot_wasm';
-export default init;
+import wasmInit from '../../../stubot-wasm/pkg/stubot_wasm';
+import wasm from '../../../stubot-wasm/pkg/stubot_wasm_bg.wasm?url';
+export default function init() {
+  return wasmInit(new URL(wasm, `${self.location}`));
+}
 
 export * from '../../../stubot-wasm/pkg/stubot_wasm';
